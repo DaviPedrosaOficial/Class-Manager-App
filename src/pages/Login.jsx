9a5906616b34c835dba8f70e4a5348015b9a25b2
Davi Pedrosa <davipedrosassss@gmail.com>
2026-04-22 12:55:06 -0300

    Enviando as primeiras atualizações de nossas pages Login & Register

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,26 @@
+import { Link, useNavigate } from "react-router-dom";
+
 function Login() {
-    return <h1>Login</h1>
+    const navigate = useNavigate();
+
+    function handleLogin() {
+        navigate("/dashboard");
+    }
+
+    return (
+        <div>
+            <h1>Login</h1>
+
+            <input className="inputLogin" type="text" placeholder="E-mail" />
+            <input className="inputLogin" type="password" placeholder="Password" />
+
+            <button className="btnLogin" onClick={handleLogin}>
+                Login
+            </button>
+
+            <p>Ainda não tem uma conta? <Link to="/register"> Cadastre-se </Link></p>
+        </div>
+    )
 }
 
 export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,25 @@
+import { useNavigate } from "react-router-dom";
+
 function Register() {
-    return <h1>Register</h1>
+    const navigate = useNavigate();
+
+    function handleRegister() {
+        navigate("/dashboard");
+    }
+
+    return (
+        <div>
+            <h1>Register</h1>
+
+            <input className="inputRegister" type="text" placeholder="Nome Completo" />
+            <input className="inputRegister" type="text" placeholder="E-mail" />
+            <input className="inputRegister" type="password" placeholder="Password" />
+
+            <button className="btnRegister" onClick={handleRegister}>
+                Register
+            </button>
+        </div>
+    )
 }
 
 export default Register;
