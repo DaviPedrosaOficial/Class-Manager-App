4e6c5ab38a800e822f122067d83fcba3c6a76df8
Davi Pedrosa <davipedrosassss@gmail.com>
2026-04-24 16:00:00 -0300

    Criado o nosso token de autenticação de usuário, que irá gerar o token para o mesmo, ao logar em nosso site

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -18,7 +18,7 @@ function Login() {
             const response = await fetch("http://localhost:3000/api/users/login", {
                 method: "POST",
                 headers: {
-                    "Content-Type": "application/json"
+                    "Content-Type": "application/json",
                 },
                 body: JSON.stringify({ email, password })
             });
